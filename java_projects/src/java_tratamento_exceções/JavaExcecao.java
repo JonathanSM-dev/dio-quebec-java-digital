b7b5dfeecad8f654e6bd41b2f08132357b3a62a2
JonathanSM-dev <jonathansilva.ans@gmail.com>
2022-09-15 16:19:13 -0300

    java exceptions

---
+++ java_projects/src/java_tratamento_exceções/JavaExcecao.java
@@ -0,0 +1,18 @@
+package java_tratamento_exceções;
+
+public class JavaExcecao {
+    public static void main(String[] args) {
+
+        // A instrução try, permite que você defina um bloco de código, para ser testado quanto a erros enquanto está sendo executado.
+        // A instrução catch, permite definir um bloco de código a ser executado, caso ocorra um erro no bloco try.
+        // A instrução finally, permite definir um bloco de código a ser executado, independente de ocorrer um erro ou não.
+        //try {
+              //  bloco de código conforme esperado
+        //}
+        //catch(Exception e) {// precisamos saber qual exceção
+              // bloco de código que captura as exceções que podem acontecer
+              // em caso de um fluxo não previsto
+        //}
+
+    }
+}
